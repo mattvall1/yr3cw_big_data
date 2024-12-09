7d85585cb533606bc4afefd57192ba8575414f60
Matthew Vallance <matthew.vallance@outlook.com>
2024-12-09 17:11:32 +0000

    Pie chart of adults and children

--- hotel_information_visualisation/hotel_visualizations.py
+++ hotel_information_visualisation/hotel_visualizations.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import tight_layout
 
 # Read the CSV file into a DataFrame
 hotel_data = pd.read_csv('data/hotel_data.csv', parse_dates=['reservation_date'])
@@ -29,6 +30,18 @@ plt.grid(True)
 plt.xticks(hotel_data['reservation_date'][::5], rotation=90)
 plt.xlim(hotel_data['reservation_date'].min(), hotel_data['reservation_date'].max()) # Set the x-axis limits to the min and max dates (keeps chart tight)
 plt.savefig("outputs/reservations.svg", format="svg", transparent=False)
+# Clear the plot
+plt.clf()
+
+# Plot a pie chart of the number of adults and children in the dataset
+labels = ['Adults', 'Children']
+sizes = [hotel_data['adults'].sum(), hotel_data['children'].sum()]
+colors = ['#80CEFF', '#ACFF80']
+plt.figure(figsize=(5, 5), tight_layout=True)
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.0f%%', startangle=90)
+plt.axis('equal')
+plt.title('Proportion of adults to children')
+plt.savefig("outputs/adults_vs_children.svg", format="svg", transparent=False)
 # Clear the plot
 plt.clf()
 
